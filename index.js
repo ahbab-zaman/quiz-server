d804fd5ee0e05ff88b043db2355f449a3bb11caa
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-06-16 21:17:59 +0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+require("dotenv").config();
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-dotenv.config();
 const app = express();
-
+app.use(express.json());
 app.use(
   cors({
     origin: [
@@ -16,12 +15,12 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 connectDB();
 
 app.use("/api/quiz", require("./routes/quizRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
