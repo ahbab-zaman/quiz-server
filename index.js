2c545b457c9a7ca0b36c88911e808ddca102cb25
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-06-16 16:53:00 +0600

    fixing error

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-dotenv.config();
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-
+dotenv.config();
 const app = express();
 
-const corsOptions = {
-  origin: "https://digitalize-client.vercel.app",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://digitalize-client.vercel.app",
+      "https://bucolic-sundae-2653c6.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-connectDB(); // If using MongoDB, else you can remove this
+connectDB();
 
-app.use("/quiz", require("./routes/quizRoutes"));
+app.use("/api/quiz", require("./routes/quizRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
